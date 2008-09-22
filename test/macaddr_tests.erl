25e5a3be363a40f8e681b727f695bb522ecc06d2
U-MTC_Office_1\mmullis <mmullis@MTC_Office_1.(none)>
2008-09-22 12:04:06 -0400

    initial revision

---
+++ test/macaddr_tests.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% File    : macaddr_basic_test.erl
+%%% Author  : Michael Mullis <michael@mullistechnologies.com>
+%%% Description : EUnit tests for Macaddr module
+%%% Created : 22 Sep 2008 by Michael Mullis <michael@mullistechnologies.com>
+%%%-------------------------------------------------------------------
+-module(macaddr_tests).
+-author("michael@mullistechnologies.com").
+
+-include_lib("eunit/include/eunit.hrl").
+
+all_test_() ->
+    {generator, macaddr_basic_test, basic_test_}.
